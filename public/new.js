4a4cf90fe85ea95ac9e1d67a0dee38f94b67d990
Atishey Jain <64005315+atisheyJain03@users.noreply.github.com>
2020-11-18 20:13:35 +0530

    Update new.js

--- public/new.js
+++ public/new.js
@@ -15,46 +15,6 @@ var data = `<!DOCTYPE html>
   </html>
   `;
 
-// var css = "body{ background-color: black;}";
-// var temp = window.frames["iframe_id"].contentWindow.document;
-// document.getElementsByTagName("iframe")[0].setAttribute("srcdoc", `${val}`);
-
-// script = "<script> console.log('hiiiiii')  </script>";
-
-// style = document.createElement("style");
-// style.innerHTML = css;
-// //var style = " <style> body {  background-color: blue;} </style>  ";
-// var doc = document.getElementById("iframe_id").contentWindow.document;
-// // doc.open();
-// // doc.write(style);
-// // doc.write(script);
-// // doc.close();
-
-// var runHtml = function () {
-//   val = document.querySelector("#html").value;
-//   style = document.querySelector("#css").value;
-//   document.getElementsByTagName("iframe")[0].setAttribute("srcdoc", `${val}`);
-// };
-
-// var run = function () {
-//   val = document.querySelector("#html").value;
-//   //
-
-//   //   doc.open();
-//   //   doc.write(style);
-//   //   doc.close();
-
-//   document.getElementsByTagName("iframe")[0].setAttribute("srcdoc", `${val}`);
-
-//   //document.getElementsByTagName("iframe")[0].setAttribute("srcdoc", `${val}`);
-//   doc.open();
-//   var style = document.createElement("style");
-//   style.innerHTML = css;
-//   //temp.getElementsByTagName("body")[0].appendChild(script);
-//   temp.getElementsByTagName("head")[0].appendChild(style);
-//   doc.close();
-// };
-
 document.querySelector(".codes").addEventListener("keyup", function (event) {
   if (event.keyCode === 13) {
     val = document.querySelector("#html").value;
